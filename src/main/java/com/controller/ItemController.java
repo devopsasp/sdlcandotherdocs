e0bd3abfe02c7f180655415df3992e77718e2a92
devopsasp <peterone23four@gmail.com>
2023-08-24 11:36:43 +0530

    all files

---
+++ src/main/java/com/controller/ItemController.java
@@ -0,0 +1,88 @@
+package com.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import com.model.Item;
+import com.service.ItemService;
+
+@RestController
+public class ItemController {
+	
+@Autowired
+ItemService itemService;
+
+@PostMapping("/item")
+public ResponseEntity<?> addItem(@RequestBody Item item)
+{
+	itemService.addItem(item);
+	
+	return new ResponseEntity("item added",HttpStatus.CREATED);
+	
+	
+}
+
+@GetMapping("/item")
+public ResponseEntity<?> getAllItems()
+{
+	List<Item> itemList=itemService.getAllItems();
+	
+	
+	return new ResponseEntity<List<Item>>(itemList,HttpStatus.OK);
+}
+
+
+@PatchMapping("/item")
+public ResponseEntity<?> updateItem(@RequestBody Item item)
+{
+
+	
+	itemService.updateItem(item);
+	return new ResponseEntity("Item updated successfully",HttpStatus.ACCEPTED);
+			
+}
+
+@DeleteMapping("/item")
+public ResponseEntity<?> deleteItem(@RequestBody Item item)
+{
+	itemService.deleteItem(item);
+	return new ResponseEntity("Item deleted successfully",HttpStatus.OK);
+}
+
+
+@GetMapping("/item/{id}")
+public ResponseEntity<?> getItemById(@PathVariable int id)
+{
+	Item item=itemService.getItemById(id);
+			return new ResponseEntity<>(item,HttpStatus.FOUND);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/service/ItemService.java
@@ -0,0 +1,60 @@
+package com.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.repository.ItemRepository;
+import com.model.Item;
+@Service
+public class ItemService {
+	
+	@Autowired
+	 ItemRepository itemRepository;
+	
+	
+	  public void addItem(Item item)
+	  {
+		  itemRepository.save(item);
+	  }
+	  
+	  public void updateItem(Item item)
+	  {
+		  itemRepository.save(item);
+	  }
+	  
+	  public void deleteItem(Item item)
+	  {
+		  itemRepository.delete(item);
+	  }
+	  public List<Item> getAllItems()
+	  {
+		
+		  ArrayList<Item> itemlist=new ArrayList();
+		  
+		return   itemRepository.findAll();
+		  
+		  
+	  }
+	  
+	  public Item getItemById(int id)
+	  {
+		return itemRepository.findById(id).get();  
+	  } 
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+
+}

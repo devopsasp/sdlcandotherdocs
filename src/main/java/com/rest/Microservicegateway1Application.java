e41d1560faf9a55bf370a256cf37eb604fd19455
devopsasp <peterone23four@gmail.com>
2023-08-28 12:42:59 +0530

    all files

--- src/main/java/com/rest/Microservicegateway1Application.java
+++ src/main/java/com/rest/Microservicegateway1Application.java
@@ -1,9 +1,15 @@
 package com.rest;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.annotation.TagValueResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import brave.baggage.BaggageField;
 
 @SpringBootApplication
 public class Microservicegateway1Application {
@@ -17,4 +23,38 @@ public class Microservicegateway1Application {
 	{
 		return new RestTemplate();
 	}
-}
+	
+	
+	@Bean
+	BaggageField getBaggage()	
+	{
+		return BaggageField.create("mybaggage");
+	}
+	
+	
+	
+	
+	@Bean 
+	TagValueResolver getResolver()
+	{
+		return parameter->"time taken:"+new java.util.Date().getTime();
+	}
+	
+	}
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/rest/MyController.java
+++ src/main/java/com/rest/MyController.java
@@ -2,25 +2,39 @@ package com.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.sleuth.SpanName;
+import org.springframework.cloud.sleuth.annotation.ContinueSpan;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
+import org.springframework.cloud.sleuth.annotation.SpanTag;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import brave.baggage.BaggageField;
+import org.springframework.cloud.sleuth.annotation.TagValueResolver;
 import java.util.*;
 import org.springframework.core.ParameterizedTypeReference;
 @RestController
+
 public class MyController {
 	
 	@Autowired
 	RestTemplate restTemplate;
+	
+	@Autowired
+	BaggageField myBaggage;
 	 
+
+	
 	@GetMapping("/")
-	@SpanName(value = "homemethod")
-	public String home()
+
+	@NewSpan("myspan")
+	//@ContinueSpan
+	public String home(@SpanTag(key="testtag1",resolver=TagValueResolver.class) String param)
 	{
- 
+ myBaggage.updateValue("new-value");
  return "rest data accessed";
 	}
 
